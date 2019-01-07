4dc9a74840c5dfeddebb22af1f5ed93672ea0e15
sonia <amirisomayeh74@gmail.com>
2019-01-07 05:17:14 -0800

    first init

--- test/test.js
+++ test/test.js
@@ -1,2 +1,29 @@
 let assert = require('chai').assert;
 
+
+
+let result;
+let arr = [0, 0, 0, 1];
+function binaryArrayToNumber(arr){
+  for (let value of arr) {
+    result = arr.map(value=>value.)
+    .join('').toString();
+  }
+  return result;
+}
+
+console.log( binaryArrayToNumber(arr));
+
+
+let arr1 =[1,2];
+let arr2 =[1];
+function  array_diff(arr1, arr2) {
+    for(let i=0;i<arr2.length;i++){
+      if(arr2[1]===arr1[1]){
+        let result =arr2.pop();
+      }
+    }
+    return result;
+}
+
+array_diff(arr1,arr2);
